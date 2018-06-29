acb02abf6160e9e6c282e14da34097357efd7f23
DaeunGod <dcj4655@naver.com>
2018-06-29 14:31:24 +0900

    Initial commit

---
+++ LibTest/LibTest.h
@@ -0,0 +1,19 @@
+//
+//  LibTest.h
+//  LibTest
+//
+//  Created by 최대운 on 2018. 6. 29..
+//  Copyright © 2018년 DaeunGod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibTest.
+FOUNDATION_EXPORT double LibTestVersionNumber;
+
+//! Project version string for LibTest.
+FOUNDATION_EXPORT const unsigned char LibTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibTest/PublicHeader.h>
+
+
